b5bd6dd10b0fc24398b055cad6bc034451523d62
olo90632951 <olo90632951@gmail.com>
2022-05-02 20:09:20 +0900

    (LV2) 피로도 (2번째 풀이)

--- 프로그래머스/위클리 챌린지/(LV2) 위클리챌린지3.py
+++ 프로그래머스/위클리 챌린지/(LV2) 위클리챌린지3.py
@@ -2,7 +2,7 @@
 # k라는 피로도를 사용해서 던전을 탐험할 때, 
 # [최소 필요 피로도,소모 피로도]가 있을 때 던전 하나당  k가 최소필요피로도보다 같거나 커야 지나갈 수 있으며 지나간 후에 k피로도에서 소모피로도만큼 뺀다 
 # 최대 몇개의 던전을 지나갈 수 있는지 구하기 (어떤 던전부터 가야하는지에 따라 달라진다)
-
+'''
 from itertools import permutations
 
 def solv(dungeon,kk,count):
@@ -25,6 +25,25 @@ def solution(k, dungeons):
     return max(answer)
 
 print(solution(80,[[80,20],[50,40],[30,10]] ))
-
+'''
 #Point
 #조합으로 푸는 문제!! 
+
+#2번째 풀기
+from itertools import permutations
+
+def solution(k, dungeons):
+    permu = list(permutations(dungeons,len(dungeons)))
+
+    answer = []
+    for per in permu:
+        count = 0
+        k1 = k
+        for a,b in per:
+            if k1 >= a:
+                k1 = k1-b
+                count += 1
+        
+        answer.append(count)
+    
+    return max(answer)
