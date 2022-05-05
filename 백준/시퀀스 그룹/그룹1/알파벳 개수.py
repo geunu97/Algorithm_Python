9c9ba969c2153ca7041706364ad47d3f4a3dfa63
olo90632951 <olo90632951@gmail.com>
2022-05-05 12:51:11 +0900

    알파벳 개수

---
+++ 백준/시퀀스 그룹/그룹1/알파벳 개수.py
@@ -0,0 +1,14 @@
+s = input()
+
+list_a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+answer = []
+for i in list_a:
+  answer.append(s.count(i))
+
+for i in answer:
+  print(i,end=" ")
+
+
+
+
