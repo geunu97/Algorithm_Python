dfa19278bddec369c627adcdb846f5400f92b46c
olo90632951 <olo90632951@gmail.com>
2022-05-06 10:09:05 +0900

    (LV2) 게임 맵 최단거리 (2번째 풀이)

--- 프로그래머스/찾아라 프로그래밍 마에스터/(LV2) 게임 맵 최단거리.py
+++ 프로그래머스/찾아라 프로그래밍 마에스터/(LV2) 게임 맵 최단거리.py
@@ -1,5 +1,5 @@
 #LV2 게임 맵 최단거리
-
+'''
 from collections import deque
 
 def solution(maps):
@@ -29,8 +29,34 @@ def solution(maps):
         return maps[-1][-1]
 
 print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
-
+'''
 
 #Point
 #BFS 문제, 최단거리 문제, 이전 미로탈출 문제와 같은문제, 기본 문제
 
+
+#2번째 풀기
+
+from collections import deque
+
+def solution(maps):
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+    
+    q = deque([[0,0]])
+    while q:
+        x,y = q.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]):
+                if maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[x][y] + 1
+                    q.append([nx,ny])
+        
+    if maps[-1][-1] == 1:
+        return -1
+    else:
+        return maps[-1][-1]
