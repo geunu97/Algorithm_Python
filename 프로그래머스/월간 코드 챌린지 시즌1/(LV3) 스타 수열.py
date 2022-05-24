d06a08e48c0b5aa5edfd98af733851d098d238d8
olo90632951 <olo90632951@gmail.com>
2022-05-24 22:01:36 +0900

    (LV3) 스타 수열

--- 프로그래머스/월간 코드 챌린지 시즌1/(LV3) 스타 수열.py
+++ 프로그래머스/월간 코드 챌린지 시즌1/(LV3) 스타 수열.py
@@ -1,27 +1,25 @@
 #LV3 스타 수열
 
-def solv(list_a):        
-    if (list_a.count(list_a[0]) == len(list_a) // 2) or (list_a.count(list_a[1]) == len(list_a) // 2):
-        return True
-    else:
-        return False
-        
+from collections import Counter
+
 def solution(a):
-    dp = [0] * (len(a)+1)
+    #print(Counter(a))
     
-    for i in range(2,len(a)+1,2):
-        if a[i-1] != a[i]:
-            if solv(a[:i]):
-                dp[i] = dp[i-2] + 2
-            else:
-                dp[i] = dp[i-2]
-        else:
-            dp[i] = dp[i-2]
+    max_value = Counter(a).most_common(1)[0][0]
+    #print(max_value)
+    
+    answer = 0
+    for i in range(0,len(a)-1,2):
+        if (a[i] == max_value or a[i+1] == max_value) and (a[i] != a[i+1]):
+            answer += 2
+    
+    print(answer)
+    return answer
+    
+
         
-    return dp[-1]
 
+        
+#각 배열에 해당 원소가 몇번 등장했는지가 중요하다. 많이 등장할수록 스타 수열의 길이가 길어질 수도 있기 때문에
 
-#?????????????
-
-
-#그냥 다시 거꾸로 카카오부터 풀까!???
+#Counter: 몇 번 등장했는지 구해서, 딕셔너리 형태로 반환된다
