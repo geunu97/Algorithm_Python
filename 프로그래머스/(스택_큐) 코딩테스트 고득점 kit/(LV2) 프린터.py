4e97a720f9f14d3f4412b821d32be7c42800a8ce
olo90632951 <olo90632951@gmail.com>
2022-06-02 20:19:21 +0900

    (LV2) 프린터 (2번쨰 풀이)

--- 프로그래머스/(스택_큐) 코딩테스트 고득점 kit/(LV2) 프린터.py
+++ 프로그래머스/(스택_큐) 코딩테스트 고득점 kit/(LV2) 프린터.py
@@ -36,3 +36,40 @@ def solution(priorities, location):
 #최댓값이 맨앞에 왔다고 무작정 그대로 return하면 안된다      #(ex)  9 3 5 1 이렇게 이대로 바로 출력하면 안된다)
 #최댓값이 맨 앞에 왔을 때 맨 앞값만 빼는 것이고, 나머지는 반복해서 진행된다      (ex) 9뽑고 다시 반복 -> 5뽑고 다시 반복 -> 3뽑고 다시 반복 -> 1뽑기)
 #                                                                              #그렇다고 정렬해서 풀면 안됨, 동일한 값 1 1 1일 때 순서가 뒤바뀜
+
+
+#2번째 풀이
+from collections import deque
+
+def solution(priorities, location):
+    prior = deque([])
+    for i in range(len(priorities)):
+        if i == location:
+            prior.append([priorities[i],1])
+        else:
+            prior.append([priorities[i],0])
+
+    answer = []
+    while True:
+        if len(prior) == 0:
+            break
+        
+        check = 1
+        value = prior.popleft()
+        for a,b in prior:
+            if value[0] < a:
+                prior.append(value)
+                check = 0
+                break
+        
+        if check == 1:
+            answer.append(value)
+    
+    count = 1
+    for a,b in answer:
+        if b == 1:
+            return count
+                          
+        count += 1
+    
+        
