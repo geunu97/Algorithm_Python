7a33fe8b592a9c4823556be8319627e9b1e72f6e
olo90632951 <olo90632951@gmail.com>
2022-06-02 15:43:13 +0900

    (LV2) 다리를 지나는 트럭 (2번째 풀이)

--- 프로그래머스/(스택_큐) 코딩테스트 고득점 kit/(LV2) 다리를 지나는 트럭.py
+++ 프로그래머스/(스택_큐) 코딩테스트 고득점 kit/(LV2) 다리를 지나는 트럭.py
@@ -3,29 +3,26 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    bridge = [0] * bridge_length
-    bridge = deque(bridge)
-
-    answer = 0
-    count = 0
-    for i in truck_weights:
+    
+    q_bridge = deque([0] * bridge_length)
+    time_value = 0
+    sum_value = 0
+    
+    for truck in truck_weights:
         while True:
-            answer += 1
-            value = bridge.popleft()
-            if value != 0:
-                count -= 1
+            sum_value -= q_bridge.popleft()     #다리에서 무조건 1초에 한칸씩 이동 
+            time_value += 1
             
-            if sum(bridge) + i <= weight and count < bridge_length:
-                bridge.append(i)
-                count += 1
-                break
+            if sum_value + truck > weight:
+                q_bridge.append(0)
             else:
-                bridge.append(0)
-    
-    
-    return bridge_length + answer    #마지막 남아있는 거 처리
+                q_bridge.append(truck)
+                sum_value += truck
+                break
+            
+    return len(q_bridge) + time_value   #마지막 남아있는 거 처리
+            
 
+#큐 문제
 
-#Point (큐 문제)
-#문제가 쪼금 이상?   
-#1초에 한칸씩 이동한다는 말이 없음 -> 그래서 예제2에서 헷갈렸음
+#문제 잘 이해해야 됨 (다리의 길이가 정해져 있고, 다리에서 1초에 한칸씩 이동하는 것 [0,0])   
