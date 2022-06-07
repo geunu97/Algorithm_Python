01733d806dcd07842585e2f744ee7cf38604cb14
olo90632951 <olo90632951@gmail.com>
2022-06-07 11:36:44 +0900

    (LV3) 이중우선순위큐 (2번째 풀이)

--- 프로그래머스/(힙) 코딩테스트 고득점 kit/(LV3) 이중우선순위큐.py
+++ 프로그래머스/(힙) 코딩테스트 고득점 kit/(LV3) 이중우선순위큐.py
@@ -10,7 +10,7 @@ def solution(operations):
         if i[0] == 'I':
             list_a.append(int(i[1]))
             list_a = sorted(list_a)
-            list_a = deque(list_a)    #덱을 정렬하면 리스트로 변환되기 때문
+            list_a = deque(list_a)    
         
         elif i[1] == '1':
             if len(list_a) == 0:
@@ -31,7 +31,30 @@ def solution(operations):
         return [list_a[-1],list_a[0]]
 
 
-#Point
-#힙으로 풀지 말고, 큐로 푸는 문제
-#넣을 때는 넣고 정렬해주기
-#뺄 때는 최소값 가장 왼쪽에서, 최댓값 가장 오른쪽에서 빼주면 됨
+#2번째 풀이
+from collections import deque
+
+def solution(operations):
+    q = deque([])
+    
+    for i in operations:
+        i = i.split(' ')
+
+        if i[0] == 'I':
+            q.append(int(i[1]))
+            q = deque(sorted(q))          #큐에서 정렬하면 큐 풀려서, 단순리스트 됨
+        
+        elif i[1] == '1' and len(q) > 0:
+            q.pop()
+
+        elif i[1] == '-1' and len(q) > 0:
+            q.popleft()
+    
+    if len(q) == 0:
+        return [0,0]
+    else:
+        return [q[-1],q[0]]
+
+
+#1. 하나의 큐로 값을 넣을 때마다 정렬, 큐로 변환해준다
+#2. 최댓값을 뺄 때는 가장 오른쪽, 최솟값을 뺄 때는 가장 왼쪽에서 빼주면 된다
