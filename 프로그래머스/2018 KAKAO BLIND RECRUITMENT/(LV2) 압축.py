800d5e7e32a92b11e8bd94dc95e5b51339e00011
olo90632951 <olo90632951@gmail.com>
2022-06-21 01:19:07 +0900

    (LV2) 압축 (2번째 풀이)

--- 프로그래머스/2018 KAKAO BLIND RECRUITMENT/(LV2) 압축.py
+++ 프로그래머스/2018 KAKAO BLIND RECRUITMENT/(LV2) 압축.py
@@ -1,5 +1,5 @@
 #LV2 압축
-
+'''
 def solution(msg):
     list_a = [0,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     
@@ -15,7 +15,28 @@ def solution(msg):
         
         i = (j-i) + i
     
-    #print(list_a)
-    #print(answer)
+    return answer
+'''
+
+#2번째 풀이
+def solution(msg):
+    dictionary = {}
+    count = 1
+    for i in range(65,91):   #초기화
+        dictionary[chr(i)] = count
+        count += 1
+    
+    answer = []
+    w = ""
+    for i in msg:
+        w += i
+        if w not in dictionary:    #문자열 앞에서부터 사전에 해당 값이 없을 떄 찾기
+            dictionary[w] = count
+            count += 1
+            
+            answer.append(dictionary[w[:-1]])
+            w = w[-1]
+    
+    answer.append(dictionary[w])   #마지막 처리
     
     return answer
