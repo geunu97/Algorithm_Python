770578e419aaca2bc6cfdb0f4264d7fd71e18afb
olo90632951 <olo90632951@gmail.com>
2022-06-20 12:03:26 +0900

    (LV2) 캐시 (2번째 풀이)

--- 프로그래머스/2018 KAKAO BLIND RECRUITMENT/(LV2) 캐시.py
+++ 프로그래머스/2018 KAKAO BLIND RECRUITMENT/(LV2) 캐시.py
@@ -1,44 +1,48 @@
 #LV2 캐시
+ 
 
+#2번째 풀이
 from collections import deque
 
 def solution(cacheSize, cities):
-    if cacheSize == 0:        #예외처리
-        return len(cities) * 5
+    if cacheSize == 0:    #예외
+        return 5 * len(cities)
     
-    db_list = deque([]) 
+    cache = deque([])
     answer = 0
-    
     for i in cities:
-        i = i.upper()    #대소문자 구별X
+        i = i.upper()  #대소문자 구별X
         
-        if len(db_list) < cacheSize:  #길이가 아직 안채워졌으면
-            if i in db_list:
+        if len(cache) < cacheSize:
+            if i in cache:
                 answer += 1
-                db_list = list(db_list)
-                db_list.pop(db_list.index(i))    #큐일때 특정인덱스로 pop()사용 불가, 리스트로 바꿔야댐
-                db_list = deque(db_list)
+                cache = list(cache)
+                cache.pop(cache.index(i))  #큐일때 특정인덱스 제거 불가,  pop()사용 불가, 리스트로 바꿔야댐
+                cache = deque(cache)
             else:
                 answer += 5
-                
-            db_list.append(i)
-            
-        elif len(db_list) == cacheSize:   #길이가 딱 채워졌으면
-            if i in db_list:
+            cache.append(i)
+         
+        else:
+            if i in cache:
                 answer += 1
-                db_list = list(db_list)
-                db_list.pop(db_list.index(i))
-                db_list = deque(db_list)
+                cache = list(cache)
+                cache.pop(cache.index(i))
+                cache.append(i)
+                cache = deque(cache)
+                
             else:
                 answer += 5
-                db_list.popleft()
-                
-            db_list.append(i)
-
+                cache.popleft()
+                cache.append(i)
+        
     return answer
 
 
-#Point
-#캐시 교체 알고리즘 Least Recently Used: (가장 가장 최근에 사용되지 않은 것) -> db_list에 문자가 있다면 그 문자를 빼고 마지막 위치에 넣어야한다 (예외)
- 
- 
+#고정 크기의 캐시에 해당 문자열이 있는지 검사, 있으면 +1, 없으면 +5 걸리는 문제
+
+'''
+1. 고정 크기의 캐시에 특정 값이 있는지 없는지 검사합니다
+   1-1. 캐시에 특정 값이 없다면, 들어온 순서대로 pop과 append를 해줍니다 (큐, answer+5)
+   1-2. [핵심] 캐시에 특정 값이 있다면, 특정 값이 있던 인덱스 자리를 제거해주고 큐의 마지막 인덱스 자리로 교체해줍니다 (LRU, answer +1)
+'''
